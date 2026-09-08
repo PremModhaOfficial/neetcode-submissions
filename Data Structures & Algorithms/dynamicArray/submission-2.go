e9df4c8be14065b454954f3204382023c3b9fcef
Prem <118674112+PremModhaOfficial@users.noreply.github.com>
2026-09-08 21:25:03 +0530

    Add: dynamicArray - submission-2

---
+++ Data Structures & Algorithms/dynamicArray/submission-2.go
@@ -0,0 +1,61 @@
+
+type DynamicArray struct {
+	backingArray []int
+	l, c         int
+}
+
+func NewDynamicArray(capacity int) *DynamicArray {
+	return &DynamicArray{
+		backingArray: make([]int, capacity),
+		l:            0,
+		c:            capacity,
+	}
+}
+
+func (da *DynamicArray) Get(i int) int {
+	return da.backingArray[i]
+}
+
+func (da *DynamicArray) Set(i int, n int) {
+	da.backingArray[i] = n
+}
+
+
+func (da *DynamicArray) Pushback(n int) {
+	if da.c > da.l {
+		// we do simple
+		da.backingArray[da.l] = n
+		da.l++
+	} else {
+		da.resize()
+		da.Pushback(n)
+	}
+}
+
+func (da *DynamicArray) Popback() int {
+	last := da.backingArray[da.l-1]
+	da.l -= 1
+	return last
+
+}
+
+func (da *DynamicArray) resize() {
+	init := len(da.backingArray)
+	newBa := make([]int, 2*init)
+	da.l = da.c
+	da.c = 2 * init
+
+	for i, el := range da.backingArray {
+		newBa[i] = el
+	}
+
+	da.backingArray = newBa
+}
+
+func (da *DynamicArray) GetSize() int {
+	return da.l
+}
+
+func (da *DynamicArray) GetCapacity() int {
+	return da.c
+}
