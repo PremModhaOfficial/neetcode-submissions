bd1d12a15fc9a2032517dbbeb47f60cbde3ab53b
Prem <118674112+PremModhaOfficial@users.noreply.github.com>
2026-09-20 19:01:16 +0530

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.go
@@ -0,0 +1,18 @@
+func search(nums []int, target int) int {
+    l, r := 0, len(nums)
+
+    var mid int
+    for l < r {
+        mid = (l+r)/2
+        switch {
+            case nums[mid] > target:
+                r = mid
+            case nums[mid] < target:
+                l = mid+1
+            default:
+                return mid
+        }
+    }
+        return -1
+
+}
